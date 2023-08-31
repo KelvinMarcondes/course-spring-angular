499196b6d7181a711266aea2dd9c0dd5094d1d1e
Kelvin Marcondes <kelvindesouza@hotmail.com>
2023-08-31 12:19:09 -0300

    projeto iniciado

---
+++ src/main/java/com/marcondes/CourseSpringAngularApplication.java
@@ -0,0 +1,13 @@
+package com.marcondes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpringAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSpringAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcondes/CourseSpringAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcondes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpringAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
